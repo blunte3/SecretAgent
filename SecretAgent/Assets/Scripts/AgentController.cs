d87ad91a638fbb5666ade6e6a5c9f9f64efeb4a4
blunte3 <csblunt030@gmail.com>
2024-02-13 15:55:28 -0500

    Pellet Grabber Singular Train
    Working pellet grabbing cube that is trained for gathering horizontal direction  in one environment

---
+++ SecretAgent/Assets/Scripts/AgentController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class AgentController : Agent
+{
+    [SerializeField] private Transform target;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(0f, 0.4f, 0f);
+
+        int rand = Random.Range(0, 2);
+        if(rand == 0)
+        {
+            target.localPosition = new Vector3(-4f, 0.4f, 0f);
+        }
+        if (rand == 1)
+        {
+            target.localPosition = new Vector3(4f, 0.4f, 0f);
+        }
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(target.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float move = actions.ContinuousActions[0];
+        float moveSpeed = 2f;
+
+        transform.localPosition += new Vector3(move, 0f) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Pellet")
+        {
+            AddReward(10f);
+            EndEpisode();
+        }
+        if (other.gameObject.tag == "Wall")
+        {
+            AddReward(-5f);
+            EndEpisode();
+        }
+    }
+}
