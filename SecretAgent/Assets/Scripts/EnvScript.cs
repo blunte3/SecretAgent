a5da26e466dde367c0ac37c26e3116c37eedf768
AbhirupD14 <137962062+AbhirupD14@users.noreply.github.com>
2024-02-16 17:41:59 -0500

    Env Script
    Wrote script to add agents to a list and check their hunger, making sure that they are removed from the scene when hunger is 0. Also moved pellet stuff to env script. There is pellet stuff still within the agent controller.

--- SecretAgent/Assets/Scripts/EnvScript.cs
+++ SecretAgent/Assets/Scripts/EnvScript.cs
@@ -4,14 +4,105 @@ using UnityEngine;
 
 public class EnvScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    //pellet variables
+    [SerializeField] private Transform target;
+    public int pelletCount;
+    public GameObject food;
+    //store pellets in this list
+     [SerializeField] public List<GameObject> spawnedPelletsList = new List<GameObject>();
+    public List<float> distanceList = new List<float>();
+    public List<float> badDistanceList = new List<float>();
+
+    //agent tracking
+    public GameObject agent;
+    [SerializeField] public List<GameObject> spawnedAgentsList = new List<GameObject>();
+
+    private void AddAgent(){
+
+        spawnedAgentsList.Add(agent);
     }
 
+    //loop through the list of Agents, checking all their hunger
+    //if hunger <= 0, delete them from list and delete the object
 
-    // Update is called once per frame
+    private void CheckAgentHunger(){
+        foreach(GameObject i in spawnedAgentsList){
+            if(i.currentHunger <= 0){
+                Destroy(i.gameObject);
+                spawnedAgentsList.Remove(spawnedAgentsList[i]);
+            }
+        }
+    }
+
+    private void CreatePellet()
+    {
+        distanceList.Clear();
+        badDistanceList.Clear();
+
+        if(spawnedPelletsList.Count != 0)
+        {
+            RemovePellet(spawnedPelletsList);
+        }
+
+        for(int i = 0; i < pelletCount; i++)
+        {
+            int counter = 0;
+            bool distanceGood;
+            bool alreadyDecremented = false;
+
+            //Spawning pellet
+            GameObject newPellet = Instantiate(food);
+            //Make pellet child of the environment
+            newPellet.transform.parent = environmentLocation;
+            //Give random spawn location
+            Vector3 pelletLocation = new Vector3(Random.Range(-4f, 4f), 0.4f, Random.Range(-4f, 4f));
+
+            if(spawnedPelletsList.Count != 0)
+            {
+                for(int k = 0; k < spawnedPelletsList.Count; k++)
+                {
+                    if(counter < 10)
+                    {
+                        distanceGood = CheckOverlap(pelletLocation, spawnedPelletsList[k].transform.localPosition, 5f);
+                        if(distanceGood == false)
+                        {
+                            pelletLocation = new Vector3(Random.Range(-4f, 4f), 0.4f, Random.Range(-4f, 4f));
+                            k--;
+                            alreadyDecremented = true;
+                        }
+
+                        distanceGood = CheckOverlap(pelletLocation, transform.localPosition, 5f);
+                        if (distanceGood == false)
+                        {
+                            pelletLocation = new Vector3(Random.Range(-4f, 4f), 0.4f, Random.Range(-4f, 4f));
+                            if(alreadyDecremented == false)
+                            {
+                                k--;
+                            }
+                        }
+
+                        counter++;
+                    }
+                    else
+                    {
+                        k = spawnedPelletsList.Count;
+                    }
+                }
+            }
+
+            //Spawn in new location
+            newPellet.transform.localPosition = pelletLocation;
+            //Add to list
+            spawnedPelletsList.Add(newPellet);
+        }
+    }
+
+    public override void OnEpisodeBegin(){
+        AddAgent();
+        CreatePellet();
+        CheckAgentHunger();
+    }
+        // Update is called once per frame
     void Update()
     {
         
